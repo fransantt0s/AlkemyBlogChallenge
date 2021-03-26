56ffa7f4e0e915865afc9a75a034c56e2f55f5c4
fransantt0s <31458991+fransantt0s@users.noreply.github.com>
2021-03-26 00:33:03 -0300

    primera Iteracion casi completa (#1)

--- src/main/java/alkemy/service/PostServiceImpl.java
+++ src/main/java/alkemy/service/PostServiceImpl.java
@@ -1,5 +1,6 @@
 package alkemy.service;
 
+import alkemy.PostNotFoundException;
 import alkemy.dao.PostDao;
 import alkemy.domain.Post;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.webjars.NotFoundException;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -32,7 +34,7 @@ public class PostServiceImpl implements PostService {
     @Transactional
     public void borrarPost(Post post) {
         if(post==null){
-            throw  new NotFoundException("El post que desea borrar no existe");
+            throw new PostNotFoundException("El post que desea borrar no existe");
         }
         postDao.delete(post);
 
@@ -42,11 +44,36 @@ public class PostServiceImpl implements PostService {
     @Transactional(readOnly = true)
     public Post encontrarPost(Post post) {
         if(post==null){
-            throw new NotFoundException("El post que desea visitrar no existe");
+            throw new PostNotFoundException("El post que desea visitar no existe");
         }
         return postDao.findById(post.getId()).orElse(null);
     }
 
+    @Override
+    @Transactional
+    public void actualizarTitulo(Long id, String titulo){
+        Post post = postDao.getOne(id);
+        if(post == null){
+            throw new PostNotFoundException("El post que desea actualizar no existe");
+        }
+        post.setCategoria(titulo);
+    }
 
+    @Override
+    public void actualizarCategoria(Long id, String categoria) {
+        Post  post= postDao.getOne(id);
+        if(post == null){
+            throw new PostNotFoundException("El post que desea actualizar no existe");
+        }
+        post.setCategoria(categoria);
+    }
+
+    @Override public void actualizarContenido(Long id, String contenido){
+        Post post = postDao.getOne(id);
+        if(post == null){
+            throw new PostNotFoundException("El post que desea actualizar no existe");
+        }
+        post.setContenido(contenido);
+    }
 
 }
